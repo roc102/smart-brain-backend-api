a589b73337712c4058574c39b21d13fab899c74b
roc102 <kyroc102@gmail.com>
2024-01-25 23:56:57 +0530

    fix

--- controllers/signin.js
+++ controllers/signin.js
@@ -2,7 +2,7 @@ const handleSignin = (User, bcrypt) => async (req, res) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
-    return res.status(400).json('Invalid credentials');
+    return res.status(400).json("Invalid credentials");
   }
 
   try {
@@ -15,19 +15,19 @@ const handleSignin = (User, bcrypt) => async (req, res) => {
 
       if (isValid) {
         // Customize the user object to be sent in the response
-        const { _id, name, email, joined } = user;
-        const userData = { _id, name, email, joined };
+        const { _id, name, email, entries, joined } = user;
+        const userData = { _id, name, email, entries, joined };
 
         return res.json(userData);
       } else {
-        return res.status(400).json('Invalid credentials');
+        return res.status(400).json("Invalid credentials");
       }
     } else {
-      return res.status(400).json('Wrong credentials');
+      return res.status(400).json("Wrong credentials");
     }
   } catch (error) {
     console.error(error);
-    return res.status(400).json('Error signing in');
+    return res.status(400).json("Error signing in");
   }
 };
 
